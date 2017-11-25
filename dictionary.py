dcd27b596f1df4030dd8884019e76390ab20cfdd
Guy Kemball-Williams <gsgkw@hotmail.co.uk>
2017-11-25 19:12:01 +0000

    can handle cases

--- dictionary.py
+++ dictionary.py
@@ -3,7 +3,11 @@ import json
 data = json.load(open("data.json"))
 
 def translate(text):
-    return data[text]
+    word = text.lower()
+    if word in data:
+        return data[word]
+    else:
+        return "Word is not found."
 
 word = input("Enter word: ")
 
