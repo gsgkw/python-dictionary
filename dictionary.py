29fd657d1bc2ae2e4395a0c20b0eac1c8afc46d5
Guy Kemball-Williams <gsgkw@hotmail.co.uk>
2017-11-28 15:44:34 +0000

    can handle proper nouns

--- dictionary.py
+++ dictionary.py
@@ -5,15 +5,18 @@ from difflib import get_close_matches
 data = json.load(open("data.json"))
 
 def translate(text):
-    word = text.lower()
-    if word in data:
-        return data[word]
+    if text in data:
+        return data[text]
     else:
-        suggestions = get_close_matches(word, data.keys())
-        if len(suggestions) > 0:
-            closest_match = suggestions[0]
-            return check_match(closest_match)
-        else: return not_found_message(word)
+        word = text.lower()
+        if word in data:
+            return data[word]
+        else:
+            suggestions = get_close_matches(word, data.keys())
+            if len(suggestions) > 0:
+                closest_match = suggestions[0]
+                return check_match(closest_match)
+            else: return not_found_message(text)
 
 def check_match(closest_match):
     yes_no = check_match_message(closest_match)
@@ -22,7 +25,7 @@ def check_match(closest_match):
     else: return not_found_message(word)
 
 def check_match_message(word):
-    return input("Did you mean '%s'? Enter Y for yes, N for no." % word)
+    return input("Did you mean '%s'? Enter Y for yes, N for no: " % word)
 
 def not_found_message(word):
     return "'%s' is not found." % word
@@ -32,5 +35,8 @@ word = input("Enter word: ")
 
 output = translate(word)
 
-for item in output:
-    print(item)
+if type(output) == list:
+    for item in output:
+        print(item)
+else:
+    print(output)
