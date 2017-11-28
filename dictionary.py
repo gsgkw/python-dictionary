d3cca559541332dcbfb7de89fbd760a982e248f1
Guy Kemball-Williams <gsgkw@hotmail.co.uk>
2017-11-28 14:45:51 +0000

    minor refactor of error message

--- dictionary.py
+++ dictionary.py
@@ -13,9 +13,10 @@ def translate(text):
         closest_match = suggestions[0]
         if len(suggestions) > 0:
             yes_no = input("Did you mean '%s'? Enter Y for yes, N for no." % closest_match)
-            if yes_no.lower() = "y":
+            if yes_no.lower() == "y":
                 return closest_match
-        else: "Word is not found."
+
+        else: "'%s' is not found." % word
 
 word = input("Enter word: ")
 
